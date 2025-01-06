0b64b02cdd8c0fa88b1eb20efffa37354097158d
ahngj <ahngijun@naver.com>
2025-01-07 00:59:44 +0900

    Update signup page

--- server.js
+++ server.js
@@ -189,3 +189,18 @@ app.get('/admin', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const signupRoute = require('./src/signup'); // signup.js 라우터
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// 회원가입 라우트 등록
+app.use('/api', signupRoute);
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
--- src/signup.js
+++ src/signup.js
@@ -1,25 +1,6 @@
-const mysql = require('mysql2');
-
-// MySQL 연결 풀 설정
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'qwqw1212!!',
-    database: 'coin_community',
-    waitForConnections: true,  // 연결이 대기 상태일 때 대기하도록 설정
-    connectionLimit: 10,      // 최대 연결 수
-    queueLimit: 0             // 연결 대기 제한 (무제한)
-});
-
-// 프로미스를 반환하는 pool 사용
-const db = pool.promise();
-
-module.exports = db;
-
-// signup.js (회원가입 처리 라우터)
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const db = require('./db');  // db.js 파일을 불러오기
+const db = require('../db');  // db.js 파일을 불러오기
 
 const router = express.Router();
 
